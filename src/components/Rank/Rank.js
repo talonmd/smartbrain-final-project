bda84d50f63530e350094c60db8013ce50ab81e4
Talon Durrant <talonmdurrant@gmail.com>
2020-05-03 02:00:00 -0600

    mostly finished with front end reactjs section of project

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Rank = () => {
+  return (
+    <div>
+     <div className='white f3'>
+      {'Talon, your current rank is...'}   
+    </div> 
+    <div className='white f1'>
+      {'#25'}   
+    </div> 
+    </div>
+  );
+};
+
+export default Rank;
