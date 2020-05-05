1ded4e5ea6f2adce3b877e1d1ccffa0d3d35ab2f
Talon Durrant <talonmdurrant@gmail.com>
2020-05-04 21:39:23 -0600

    debugging

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -44,13 +44,13 @@ render() {
           <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlfor="email-address">Email</label>
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
               <input 
               onChange={this.onEmailChange}
               className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlfor="password">Password</label>
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
               <input 
               onChange={this.onPasswordChange}
               className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
